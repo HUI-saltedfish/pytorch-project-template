8b8b5618aecf2a6b1aa6cc43c2b4df38265581fc
刘辉 <liuhui@deepwise.com>
2022-04-10 15:00:42 +0800

    first

--- utils/utils.py
+++ utils/utils.py
@@ -27,3 +27,20 @@ def get_logger(cfg, name=None):
             OmegaConf.to_container(cfg.job_logging_cfg, resolve=True)
         )
         return logging.getLogger(name)
+    
+def get_world_size():
+    if not dist.is_initialized():
+        return 1
+    return dist.get_world_size()
+
+def reduce_value(value, average=True):
+    world_size = get_world_size()
+    if world_size < 2:  # 单GPU的情况
+        return value
+
+    with torch.no_grad():
+        dist.all_reduce(value)
+        if average:
+            value /= world_size
+
+        return value
